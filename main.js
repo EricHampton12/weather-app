3290a050815fd4fa9561ab3c101974e762152ec5
Eric Hampton <erichampton2010@gmail.com>
2019-09-25 12:55:10 -0400

    finished app

--- main.js
+++ main.js
@@ -1,50 +1,100 @@
-let appId = 'bda4e1902e1235e2d8fb387df6fab50c';
-let units = 'imperial';
-let searchMethod; 'zip';
-var url = 'http://api.openweathermap.org/data/2.5/weather?&units=imperial&APPID=' + appId;
-var tempF = document.querySelector('#tempF');
-var tempC = document.querySelector('#tempC');
-var tempK = document.querySelector('#tempK')
-function getSearchMethod(searchTerm) {
-    if (searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
-        searchMethod = 'zip';
-    else
-        searchMethod = 'q';
+var zipInput = document.getElementById("zipInput");
+var weatherButton = document.getElementById("weatherButton");
+
+var error = document.getElementById("error");
+var errorMessage = document.getElementById("errorMessage");
+
+var output = document.getElementById("output");
+
+var conditionOutput = document.getElementById("conditionOutput");
+var temperatureOutputK = document.getElementById("temperatureOutputK");
+var temperatureOutputF = document.getElementById("temperatureOutputF");
+var temperatureOutputC = document.getElementById("temperatureOutputC");
+var cityOutput = document.getElementById("cityOutput");
+var weatherIcon = document.getElementById("weatherIcon");
+
+
+var apiRequest;
+
+
+
+// Wait for page to load before making button work
+document.onreadystatechange = function() {
+  if (document.readyState == "interactive") {
+    weatherButton.onclick = getWeather;
+  }
+};
+
+
+function getWeather() {
+
+  var url="https://api.openweathermap.org/data/2.5/weather?zip=<zipcode>&us&appid=bda4e1902e1235e2d8fb387df6fab50c";
+  url = url.replace("<zipcode>", zipInput.value); 
+
+  apiRequest = new XMLHttpRequest();
+  apiRequest.onload = catchResponse;
+  apiRequest.onerror = httpRequestOnError;
+  apiRequest.open('get', url, true);
+  apiRequest.send();
+
 }
 
-async function searchWeather(searchTerm) {
-    getSearchMethod(searchTerm);
-    await fetch(url + '&zip=' + searchTerm).then(result => {
-        return result.json();
 
-    }).then(result => {
-        updateUI(result);
-    })
+function catchResponse() {
+
+  if (apiRequest.statusText === "OK") {
+
+    errorMessage.innerHTML = '';
+    error.style.display = 'none';
+    output.style.display = 'block';
+
+    parseResponse();
+
+  }
+
+  else {
+
+    errorMessage.innerHTML = JSON.parse(apiRequest.responseText).message;
+    error.style.display = 'block';
+    output.style.display = 'none';
+
+  }
+  
+  console.log(JSON.parse(apiRequest.responseText));
+
+
 }
 
-function updateUI(resultFromServer) {
-   
+function parseResponse() {
 
-         
-    console.log(resultFromServer);
+
+
+    var results = JSON.parse(apiRequest.responseText);
+
+    var tempK = Math.round(results.main.temp);
+    var tempF = Math.round(9/5 * (tempK - 273) + 32); // 9/5 (K - 273) + 32
+    var tempC = tempK - 273; // K - 273
+
+    temperatureOutputK.innerHTML = tempK + "&deg;";
+    temperatureOutputF.innerHTML = tempF + "&deg;";
+    temperatureOutputC.innerHTML = tempC + "&deg;";
+
+    cityOutput.innerHTML = results.name;
+
     
-    let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
-    let temperatureElement = document.getElementById('temperature');
-    let humidityElement = document.getElementById('humidity');
-    let cityHeader = document.getElementById('cityHeader');
-    let weatherIcon = document.getElementById('img-icon');
+    conditionOutput.innerHTML = results.weather[0].description;
+    var iconSrc = results.weather[0].icon;
+    
+    weatherIcon.setAttribute("src", `http://openweathermap.org/img/w/${iconSrc}.png`);
+    
 
-    // weatherIconImg.src ='http://openweathermap.org/img/wn/10d@2x.png' + resultFromServer.weather[0],icon + '.png';
-
-    let resultDescription = resultFromServer.weather[0].description;
-
-    weatherDescriptionHeader.innerText = resultDescription;
-    tempF.innerText = resultFromServer.temperatureElement;
 }
 
-document.getElementById('searchBtn').addEventListener('click', () => {
-    let searchTerm = document.getElementById('searchInput').value;
-    if (searchTerm)
-        searchWeather(searchTerm);
-})
+function httpRequestOnError() {
 
+    errorMessage.innerHTML = "HTTP request could not be completed";
+    error.style.display = 'block';
+    output.style.display = 'none';
+
+
+}
